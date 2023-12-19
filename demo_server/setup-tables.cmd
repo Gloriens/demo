34774453af8fb71c1212af5f388dc23706e38571
Oğuzhan Çalışkan <77392426+Gloriens@users.noreply.github.com>
2023-12-19 20:17:42 +0300

    some issues
    Some issues about dependincies. I have to rebuild the project

---
+++ demo_server/setup-tables.cmd
@@ -0,0 +1,22 @@
+@echo off
+
+@REM Start docker
+echo Starting docker
+docker compose up --build --detach
+
+:LOOP
+netstat -o -n -a | >nul findstr "8090" && (
+    echo Waiting for Postgres...
+    timeout /t 2 /nobreak > NUL
+    goto :PORT_FOUND
+)
+echo Waiting for Postgres...
+goto :LOOP
+
+:PORT_FOUND
+echo Postgres is ready
+type .\generated\tables-serverpod.pgsql | docker compose run -T postgres env PGPASSWORD="Wg7K4UTjSqAoqVbak-WfkFV2yXG_lGJ4" psql -h postgres -U postgres -d demo
+echo Stopping docker
+docker compose stop
+
+exit /b
